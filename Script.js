26c537241a6f71e0b79116bad5866c66b9e6eacb
Bikash kumar Singh <128299431+bikashsinghchouhan@users.noreply.github.com>
2025-06-20 01:16:42 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,23 @@
+const hamburger=document.getElementById('hamberger');
+const closeBtn=document.getElementById('closeBtn');
+const navLinks = document.getElementById('nav-links');
+const overlay = document.getElementById('overlay');
+const navItems = navLinks.querySelectorAll('a');
+function closeMenu() {
+    navLinks.classList.remove('open');
+    overlay.classList.remove('show');
+  }
+  
+  hamburger.addEventListener('click', () => {
+    navLinks.classList.add('open');
+    overlay.classList.add('show');
+  });
+  
+  closeBtn.addEventListener('click', closeMenu);
+  overlay.addEventListener('click', closeMenu);
+  
+  // Close menu when link is clicked
+  navItems.forEach(link => {
+    link.addEventListener('click', closeMenu);
+  });
+  
