196bbdfe9e2fed6a5642c4cc1031576820be2dc3
Bikash kumar Singh <128299431+bikashsinghchouhan@users.noreply.github.com>
2025-07-02 18:39:12 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -70,3 +70,131 @@ document.addEventListener("keydown", function (e) {
   //  e.preventDefault();
    // showCustomPopup("🚫 Right-click is disabled!");
   //});
+
+
+
+
+
+
+//gsap Animation
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".leftSection", {
+  scrollTrigger: {
+    trigger: ".leftSection ",
+    toggleActions: "play reset play reset"
+  },
+  y: 30,
+  ease: "power2.out",
+  duration: 1,
+  delay: 0,
+  stagger: 0.5,
+})
+
+
+
+gsap.from(".contact, .resume", {
+  scrollTrigger: {
+    trigger: ".contact,.resume ",
+    toggleActions: "play reset play reset"
+  },
+  y: 30,
+  ease: "power2.out",
+  duration: 1,
+  delay: 0,
+  stagger: 0.5,
+})
+
+gsap.from(".rightSection img,hr", {
+  scrollTrigger: {
+    trigger: ".resume ",
+    toggleActions: "play reset play reset"
+  },
+  scale: 0.8,
+  ease: "power2.out",
+  duration: 1,
+  delay: 0,
+  stagger: 0.5,
+})
+
+
+
+
+gsap.from(".secondSection h1,h5", {
+  scrollTrigger: {
+    trigger: ".mySkils h1",
+    toggleActions: "play reset play reset"
+  },
+  y: 30,
+  ease: "power2.out",
+  opacity: 0,
+  duration: 1,
+  delay: 0,
+  stagger: 0.5,
+})
+
+gsap.from("#Education h2", {
+  scrollTrigger: {
+    trigger: "#Education h2",
+    toggleActions: "play reset play reset"
+  },
+  scale: 0.8,
+  ease: "power2.out",
+  opacity: 0,
+  duration: 1,
+  delay: 0.5
+})
+
+
+
+
+gsap.utils.toArray(".skill-card").forEach((proj) => {
+  gsap.from(proj, {
+    scrollTrigger: {
+      trigger: proj,
+      start: "top 80%", // Start when element enters viewport
+      toggleActions: "play reset play reset",
+      markers: false
+    },
+    y: 40,
+    opacity: 0,
+    duration: 0.5,
+    ease: "power2.out"
+
+  });
+});
+gsap.utils.toArray(".timeline-item").forEach((proj) => {
+  gsap.from(proj, {
+    scrollTrigger: {
+      trigger: proj,
+      start: "top 80%", // Start when element enters viewport
+      toggleActions: "play reset play reset",
+      markers: false
+    },
+    opacity: 0,
+    y: 20,
+    duration: 1,
+    delay: 0.1,
+    stagger: 1,
+    ease: "power2.out"
+
+
+  });
+});
+
+
+gsap.utils.toArray(".project-box").forEach((proj) => {
+  gsap.from(proj, {
+    scrollTrigger: {
+      trigger: proj,
+      start: "top 80%", // Start when element enters viewport
+      toggleActions: "play reset play reset",
+      markers: false
+    },
+    scale: 0.8,
+    opacity: 0.5,
+    duration: 1.5,
+    ease: "power2.out"
+
+  });
+});
